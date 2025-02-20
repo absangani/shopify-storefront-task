cde287a01517e579e49e8e362c1f0844ff4312fa
akshay.sangani <akshay.sangani@lucentinnovation.com>
2025-02-20 16:11:30 +0530

    Frontend build render setup

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,18 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const typeDefs = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
+const path = require("path");
 
 dotenv.config();
 const app = express();
 app.use(cors());
+
+// Serve Frontend Build Folder
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get(["*"], (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
